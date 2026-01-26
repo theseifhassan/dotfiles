026fd09d310673446044e8b7d35795c2ad2b34f5
theseifhassan <theseifhassan@gmail.com>
2026-01-26 20:41:30 +0200

    defaults: fix deprecated tmux command, remove redundant fzf init

--- default/zsh/init.zsh
+++ default/zsh/init.zsh
@@ -1,7 +1,4 @@
 command -v starship >/dev/null 2>&1 && eval "$(starship init zsh)"
-
-[ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
-[ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
 command -v fzf >/dev/null 2>&1 && eval "$(fzf --zsh)" 2>/dev/null
 
 command -v zoxide >/dev/null 2>&1 && eval "$(zoxide init zsh)"
