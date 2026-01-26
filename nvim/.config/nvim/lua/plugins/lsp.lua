93dda606d67a78a73887bc6e61bd98a6a7019fb8
theseifhassan <theseifhassan@gmail.com>
2026-01-26 20:41:10 +0200

    nvim: cleanup plugins, fix lsp config, remove disabled treesitter-context

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,17 @@
 return {
     {
         "neovim/nvim-lspconfig",
-        config = function()
-            vim.lsp.enable({ "lua_ls", "ts_ls", "tailwindcss", "marksman", "yamlls", "jsonls", "biome", "bashls" })
-        end
+        opts = {
+            servers = {
+                lua_ls = {},
+                ts_ls = {},
+                tailwindcss = {},
+                marksman = {},
+                yamlls = {},
+                jsonls = {},
+                biome = {},
+                bashls = {},
+            },
+        },
     },
 }
