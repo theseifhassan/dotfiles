48588f6d0d0760f24214075aa2ad90fdf6a4d07c
theseifhassan <theseifhassan@gmail.com>
2026-02-02 18:36:05 +0200

    chore: clean up unused aliases and mise tools

--- default/zsh/aliases.zsh
+++ default/zsh/aliases.zsh
@@ -1,34 +1,3 @@
 # Shell aliases
-alias ..='cd ..'
-alias ...='cd ../..'
-
-alias ls='eza'
-alias ll='eza -l'
-alias la='eza -la'
-alias lt='eza -T'
-
-alias cat='bat --style=plain'
-
-alias g='git'
-alias gs='git status'
-alias ga='git add'
-alias gc='git commit'
-alias gp='git push'
-alias gl='git pull'
-alias gd='git diff'
-alias gco='git checkout'
-alias gb='git branch'
-alias glog='git log --oneline --graph'
-
-alias lg='lazygit'
-
-alias d='docker'
-alias dc='docker compose'
-alias ld='lazydocker'
-
 alias v='nvim'
 alias vim='nvim'
-alias tm='tmux'
-alias tma='tmux attach'
-
-alias dots='cd $DOTFILES'
